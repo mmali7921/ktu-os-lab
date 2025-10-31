c017855a9c7552727cc1b2fd17b21a41f27bb04f
Muhammed Ali <mmali2004@gmail.com>
2025-11-01 00:04:54 +0530

    priority and rr fixed

--- 10-prio.c
+++ 10-prio.c
@@ -1,103 +1,101 @@
 #include <stdio.h>
-
-struct process
-{
+struct process{
     int pid;
     int at;
     int bt;
     int ct;
-    int wt;
     int tat;
-    int prio;
+    int wt;
+    int pr;
 }pro[100];
 int n;
-
-void input()
-{
-    for(int i=0;i<n;i++)
-    {
-        printf("enter the process id\n");
-        scanf("%d",&pro[i].pid);
-        printf("enter the arrival time\n");
-        scanf("%d",&pro[i].at);
-        printf("enter the burst time\n");
-        scanf("%d",&pro[i].bt);  
-        printf("enter the priority\n");
-        scanf("%d",&pro[i].prio);  
+void input(){
+    for(int i=0;i<n;i++){
+    printf("Enter the pid ");       
+    scanf("%d",&pro[i].pid);
+    printf("Enter the arrival time ");
+    scanf("%d",&pro[i].at);
+    printf("Enter the burst time ");
+    scanf("%d",&pro[i].bt);
+    printf("Enter the priority ");
+    scanf("%d",&pro[i].pr);
     }
 }
-
-void sort()
-{
-    for(int i=0;i<n-1;i++){
+void sort(){
+    struct process temp;
+    for(int i=0;i<n-1;i++){         
         for(int j=0;j<n-i-1;j++){
-            if(pro[j].at>pro[j+1].at || (pro[j].at==pro[j+1].at && pro[j].prio>pro[j+1].prio)){
-                struct process temp=pro[j];
+            if(pro[j].at > pro[j+1].at){        
+                temp=pro[j];
                 pro[j]=pro[j+1];
                 pro[j+1]=temp;
             }
         }
-    
     }
 }
 void showProcess(){
-    printf("\npid\tbt\tat\n");
+    printf("PID\tAT\tBT\tCT\tTAT\tWT\tPR\n");               
     for(int i=0;i<n;i++){
-        printf("%d\t%d\t%d\n",pro[i].pid,pro[i].at,pro[i].bt);
-
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",pro[i].pid,pro[i].at,pro[i].bt,pro[i].ct,pro[i].tat,pro[i].wt,pro[i].pr);
     }
 }
 void calCT(){
     int time=0;
-    pro[0].ct=pro[0].at+pro[0].bt;
-    time=pro[0].ct;
-
-    for(int i=1;i<n;i++){
-
-        if(pro[i].at<time){
-            pro[i].ct=time+pro[i].bt;
-            time=pro[i].ct;
+    int completed=0;
+    int done[100]={0};
+    while(completed!=n){
+        int highestPr=9999;
+        int idx=-1;
+        for(int i=0;i<n;i++){
+            if(pro[i].at<=time && done[i]==0){
+                if(pro[i].pr<highestPr){
+                    highestPr=pro[i].pr;
+                    idx=i;
+                }
+                else if(pro[i].pr==highestPr && idx!=-1 && pro[i].at<pro[idx].at){
+                    idx=i;
+                }
+            }
+        }
+        if(idx==-1){
+            time++;
         }
         else{
-            pro[i].ct=pro[i].at+pro[i].bt;
-            time=pro[i].ct;
+            pro[idx].ct=time + pro[idx].bt;
+            time=pro[idx].ct;
+            done[idx]=1;
+            completed++;
         }
     }
 }
 void calTAT(){
-    for ( int i = 0; i < n; i++)
-    {
-        pro[i].tat=pro[i].ct-pro[i].at;
+    for(int i=0;i<n;i++){
+        pro[i].tat=pro[i].ct - pro[i].at;
     }
-    
 }
 void calWT(){
-    for ( int i = 0; i < n; i++)
-    {
-        pro[i].wt=pro[i].tat-pro[i].bt;
+    for(int i=0;i<n;i++){       
+        pro[i].wt=pro[i].tat - pro[i].bt;
     }
-    
-}
-void calAvg(){
-    float avgtat=0,avgwt=0;
-    for ( int i = 0; i < n; i++)
-    {
-       avgtat+=pro[i].tat;
-       avgwt+=pro[i].wt;
+}   
+void calAVG(){
+    float totalTAT=0, totalWT=0;
+    for(int i=0;i<n;i++){
+        totalTAT+=pro[i].tat;
+        totalWT+=pro[i].wt;
     }
-    printf("avgwt=%f\navgtat=%f",avgwt/n,avgtat/n);
-}
-int main(){
-    printf("enter the number of processes\n");
-    scanf("%d",&n);
+    printf("Average TAT: %.2f\n", totalTAT/n);
+    printf("Average WT: %.2f\n", totalWT/n);
+}                        
 
+void main(){
+    printf("Enter number of processes ");
+    scanf("%d",&n);
     input();
     sort();
-    showProcess();
     calCT();
     calTAT();
     calWT();
-    calAvg();
-
-    return 0;
+    showProcess();
+    
 }
--- 11-roundrob.c
+++ 11-roundrob.c
@@ -40,6 +40,17 @@ void showProcess(){
 
     }
 }
+void sort(){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(pro[j].at>pro[j+1].at){
+                struct process temp=pro[j];
+                pro[j]=pro[j+1];
+                pro[j+1]=temp;
+            }
+        }
+    }
+}
 void calCT(){
   int done=0,time=0;
     while(done<n){
